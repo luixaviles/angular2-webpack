3d6f50bfae999c03b53ad9a18aa39423e5661df8
luixaviles <luis.avilesz@gmail.com>
2016-07-22 20:10:42 -0400

    Adding directive

---
+++ src/app/attribute-directives/highlight.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({ 
+    selector: '[myHighlight]' 
+})
+export class HighlightDirective {
+    private el: HTMLElement;
+    private _defaultColor = 'red';
+
+    constructor(el: ElementRef) {
+        this.el = el.nativeElement;
+    }
+
+    @Input()
+    set defaultColor(colorName: string) {
+        this._defaultColor = colorName || this._defaultColor;
+    }
+
+    @Input('myHighlight')
+    highlightColor: string;
+
+    @HostListener('mouseenter')
+    onMouseEnter() {
+        this.highlight(this.highlightColor || this._defaultColor);
+    }
+
+    @HostListener('mouseleave')
+    onMouseLeave() {
+        this.highlight(null);
+    }
+
+    private highlight(color: string) {
+        this.el.style.backgroundColor = color;
+    }
+}
